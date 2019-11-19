9543cfe84a01c7a6f1222d5686c519a121a72e36
zpdeng <allendeng4@gmail.com>
2019-11-18 16:38:32 -0800

    charts are up

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path 
 from . import views
+from django.conf.urls import url, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
 	path('', views.home, name='dashboard-home'),
 	path('about/', views.about, name='dashboard-about'),
 	path('charts/', views.charts, name='dashboard-charts'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
--- web_analytics/urls.py
+++ web_analytics/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('dashboard/', include('dashboard.urls')),
-	
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
